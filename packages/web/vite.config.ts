b00da0066325fa1852b820ac56531bbf20368953
Declan L. Scott <declanlscott@gmail.com>
2025-01-12 13:16:51 -0600

    feat: bundle visualizer

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,9 +1,10 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), TanStackRouterVite({}), react()],
+  plugins: [tsconfigPaths(), TanStackRouterVite({}), react(), visualizer()],
 });

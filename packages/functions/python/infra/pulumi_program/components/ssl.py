d58e5135bb31b3bbb5a7ea3d2e31a1e967e11074
Declan L. Scott <declanlscott@gmail.com>
2025-01-26 16:18:15 -0600

    fix(infra): ssl component

--- packages/functions/python/infra/pulumi_program/components/ssl.py
+++ packages/functions/python/infra/pulumi_program/components/ssl.py
@@ -3,7 +3,8 @@ import pulumi_aws as aws
 import pulumi_cloudflare as cloudflare
 from typing import Sequence
 
-from utilities import aws_region, resource, tags
+from utilities import resource, tags
+from utilities.aws import sts
 
 
 class SslArgs:
@@ -13,58 +14,95 @@ class SslArgs:
 
 class Ssl(pulumi.ComponentResource):
     def __init__(self, args: SslArgs, opts: pulumi.ResourceOptions = None):
+        super().__init__(t="pw:resource:Ssl", name="Ssl", props=vars(args), opts=opts)
+
         def create_records(
+            domain_name: pulumi.Input[str],
             options: Sequence[aws.acm.outputs.CertificateValidationOption],
         ) -> list[cloudflare.record.Record]:
             records: list[cloudflare.record.Record] = []
+
+            zone_id = cloudflare.get_zone_output(
+                name=resource["AppData"]["domainName"]["value"]
+            ).id
+
+            records.append(
+                cloudflare.Record(
+                    resource_name="CaaRecord",
+                    args=cloudflare.RecordArgs(
+                        zone_id=zone_id,
+                        type="CAA",
+                        name=domain_name,
+                        data=cloudflare.RecordDataArgs(
+                            flags="0",
+                            tag="issue",
+                            value="amazonaws.com",
+                        ),
+                        ttl=60,
+                    ),
+                    opts=pulumi.ResourceOptions(
+                        parent=self, delete_before_replace=True
+                    ),
+                )
+            )
 
             for index, option in enumerate(options):
                 records.append(
                     cloudflare.Record(
                         resource_name=f"CertificateValidationRecord{index}",
                         args=cloudflare.RecordArgs(
-                            zone_id=cloudflare.get_zone_output(
-                                name=resource["AppData"]["domainName"]["value"]
-                            ).id,
+                            zone_id=zone_id,
                             type=option.resource_record_type,
                             name=option.resource_record_name,
-                            value=option.resource_record_value,
+                            content=option.resource_record_value,
+                            ttl=60,
                         ),
-                        opts=pulumi.ResourceOptions(parent=self),
+                        opts=pulumi.ResourceOptions(
+                            parent=self, delete_before_replace=True
+                        ),
                     )
                 )
 
             return records
 
-        super().__init__(t="pw:resource:Ssl", name="Ssl", props=vars(args), opts=opts)
+        us_east_1_credentials = sts.assume_role(
+            RoleArn=resource["Aws"]["roles"]["pulumi"]["arn"],
+            RoleSessionName="InfraFunctionSslComponent",
+        )["Credentials"]
 
         us_east_1_provider = aws.Provider(
             resource_name="UsEast1Provider",
-            args=aws.ProviderArgs(region=aws_region),
+            args=aws.ProviderArgs(
+                access_key=us_east_1_credentials["AccessKeyId"],
+                secret_key=us_east_1_credentials["SecretAccessKey"],
+                token=us_east_1_credentials["SessionToken"],
+                region="us-east-1",
+            ),
             opts=pulumi.ResourceOptions(parent=self),
         )
 
         self.__certificate = aws.acm.Certificate(
             resource_name="Certificate",
             args=aws.acm.CertificateArgs(
-                domain_name=pulumi.Output.format(
-                    "{0}.backend.{1}",
-                    args.tenant_id,
-                    resource["AppData"]["domainName"]["fullyQualified"],
-                ),
+                domain_name=f"{args.tenant_id}.backend.{resource["AppData"]["domainName"]["fullyQualified"]}",
+                validation_method="DNS",
                 tags=tags(args.tenant_id),
             ),
             opts=pulumi.ResourceOptions(parent=self, provider=us_east_1_provider),
         )
 
-        self.__records: list[cloudflare.record.Record] = (
-            self.__certificate.domain_validation_options.apply(create_records)
+        self.__records: pulumi.Output[list[cloudflare.record.Record]] = (
+            self.__certificate.domain_validation_options.apply(
+                lambda options: create_records(self.__certificate.domain_name, options)
+            )
         )
 
         self.register_outputs(
             {
                 "certificate": self.__certificate.id,
-                "records": [record.id for record in self.__records],
+                "records": self.__records.apply(
+                    lambda records: [record.id for record in records]
+                ),
             }
         )
 

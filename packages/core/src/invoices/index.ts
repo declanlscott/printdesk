ea99aec910909fd27babfaadd43ceade8ba0d172
Declan L. Scott <declanlscott@gmail.com>
2025-02-12 22:41:28 -0600

    fix(core): move api call outside of db transaction

--- packages/core/src/invoices/index.ts
+++ packages/core/src/invoices/index.ts
@@ -1,9 +1,9 @@
 import { and, eq, getTableName, inArray } from "drizzle-orm";
 
 import { AccessControl } from "../access-control";
+import { Api } from "../backend/api";
 import { afterTransaction, useTransaction } from "../drizzle/context";
 import { poke } from "../replicache/poke";
-import { Api } from "../tenants/api";
 import { useTenant } from "../tenants/context";
 import { Users } from "../users";
 import { ApplicationError } from "../utils/errors";
@@ -20,20 +20,20 @@ export namespace Invoices {
       args: [{ name: getTableName(invoicesTable) }],
     });
 
+    const res = await Api.send("/invoices", {
+      method: "POST",
+      body: JSON.stringify({ invoiceId: values.id }),
+    });
+    if (!res.ok)
+      throw new Error(
+        `Failed enqueueing invoice ${values.id} for order ${values.orderId}.`,
+      );
+
     return useTransaction(async (tx) => {
       const [users] = await Promise.all([
         Users.withOrderAccess(values.orderId),
         tx.insert(invoicesTable).values(values),
       ]);
-
-      const res = await Api.send("/invoices", {
-        method: "POST",
-        body: JSON.stringify({ invoiceId: values.id }),
-      });
-      if (!res.ok)
-        throw new Error(
-          `Failed enqueueing invoice ${values.id} for order ${values.orderId}.`,
-        );
 
       await afterTransaction(() =>
         poke(users.map((u) => `/users/${u.id}` as const)),

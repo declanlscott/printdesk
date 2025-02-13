554c103386aa5fcf7b58c857cae384f718908ff9
Declan L. Scott <declanlscott@gmail.com>
2025-02-12 22:53:02 -0600

    refactor(api): actor middleware

--- packages/functions/node/src/api/middleware/actor.ts
+++ packages/functions/node/src/api/middleware/actor.ts
@@ -1,57 +1,30 @@
 import { createClient } from "@openauthjs/openauth/client";
 import { withActor } from "@printworks/core/actors/context";
 import { subjects } from "@printworks/core/auth/subjects";
-import { useTransaction } from "@printworks/core/drizzle/context";
-import { licensesTable, tenantsTable } from "@printworks/core/tenants/sql";
-import { and, eq } from "drizzle-orm";
+import { bearerAuth } from "hono/bearer-auth";
+import { every, some } from "hono/combine";
 import { createMiddleware } from "hono/factory";
-import * as R from "remeda";
 
-export const actor = createMiddleware(async (c, next) => {
-  const accessToken = c.req.header("Authorization")?.replace("Bearer ", "");
+export const actor = createMiddleware(
+  some(
+    every(
+      bearerAuth({
+        async verifyToken(token, c) {
+          const verified = await createClient({ clientID: "api" }).verify(
+            subjects,
+            token,
+          );
+          if (verified.err) {
+            console.error("Token verification failed: ", verified.err);
+            return false;
+          }
 
-  if (accessToken) {
-    const verified = await createClient({ clientID: "api" }).verify(
-      subjects,
-      accessToken,
-    );
-
-    if (!verified.err)
-      return withActor(
-        { type: "user", properties: verified.subject.properties },
-        next,
-      );
-  }
-
-  // NOTE: System actor on the api is only used for initializing tenants
-  const licenseKey = c.req.header("X-License-Key");
-  const tenantId = c.req.header("X-Tenant-Id");
-  if (licenseKey && tenantId) {
-    const result = await useTransaction((tx) =>
-      tx
-        .select({ tenantId: tenantsTable.id })
-        .from(licensesTable)
-        .innerJoin(tenantsTable, eq(licensesTable.tenantId, tenantsTable.id))
-        .where(
-          and(
-            eq(licensesTable.key, licenseKey),
-            eq(licensesTable.tenantId, tenantId),
-            eq(licensesTable.status, "active"),
-            eq(tenantsTable.status, "initializing"),
-          ),
-        )
-        .then(R.first()),
-    );
-
-    if (result)
-      return withActor(
-        {
-          type: "system",
-          properties: { tenantId: result.tenantId },
+          c.set("subject", verified.subject);
+          return true;
         },
-        next,
-      );
-  }
-
-  return withActor({ type: "public", properties: {} }, next);
-});
+      }),
+      (c, next) => withActor(c.get("subject"), next),
+    ),
+    (_, next) => withActor({ type: "public", properties: {} }, next),
+  ),
+);

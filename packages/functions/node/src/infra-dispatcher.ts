c8411148464f03e39c4b3c32024c2f23335aeb31
Declan L. Scott <declanlscott@gmail.com>
2025-03-26 23:43:18 -0500

    fix: context callback

--- packages/functions/node/src/infra-dispatcher.ts
+++ packages/functions/node/src/infra-dispatcher.ts
@@ -9,41 +9,44 @@ import * as R from "remeda";
 import { Resource } from "sst";
 
 export const handler = async () =>
-  withAws({ sqs: { client: new Sqs.Client() } }, async () => {
-    const tenants = await useTransaction((tx) =>
-      tx
-        .select({
-          id: tenantsTable.id,
-          infraProgramInput: tenantMetadataTable.infraProgramInput,
-        })
-        .from(tenantsTable)
-        .innerJoin(
-          tenantMetadataTable,
-          eq(tenantMetadataTable.tenantId, tenantsTable.id),
-        )
-        .where(eq(tenantsTable.status, "active")),
-    );
+  withAws(
+    () => ({ sqs: { client: new Sqs.Client() } }),
+    async () => {
+      const tenants = await useTransaction((tx) =>
+        tx
+          .select({
+            id: tenantsTable.id,
+            infraProgramInput: tenantMetadataTable.infraProgramInput,
+          })
+          .from(tenantsTable)
+          .innerJoin(
+            tenantMetadataTable,
+            eq(tenantMetadataTable.tenantId, tenantsTable.id),
+          )
+          .where(eq(tenantsTable.status, "active")),
+      );
 
-    const failedEntries: NonNullable<
-      Awaited<ReturnType<typeof Sqs.sendMessageBatch>>["Failed"]
-    > = [];
+      const failedEntries: NonNullable<
+        Awaited<ReturnType<typeof Sqs.sendMessageBatch>>["Failed"]
+      > = [];
 
-    for (const chunk of R.chunk(tenants, 10)) {
-      const { Failed } = await Sqs.sendMessageBatch({
-        QueueUrl: Resource.InfraQueue.url,
-        Entries: chunk.map(({ id: tenantId, infraProgramInput }, index) => ({
-          Id: index.toString(),
-          MessageBody: JSON.stringify({ tenantId, ...infraProgramInput }),
-        })),
-      });
+      for (const chunk of R.chunk(tenants, 10)) {
+        const { Failed } = await Sqs.sendMessageBatch({
+          QueueUrl: Resource.InfraQueue.url,
+          Entries: chunk.map(({ id: tenantId, infraProgramInput }, index) => ({
+            Id: index.toString(),
+            MessageBody: JSON.stringify({ tenantId, ...infraProgramInput }),
+          })),
+        });
 
-      if (Failed && !R.isEmpty(Failed)) {
-        console.error("Failed to send messages to SQS", Failed);
-        failedEntries.push(...Failed);
+        if (Failed && !R.isEmpty(Failed)) {
+          console.error("Failed to send messages to SQS", Failed);
+          failedEntries.push(...Failed);
+        }
       }
-    }
 
-    if (!R.isEmpty(failedEntries)) return { success: false };
+      if (!R.isEmpty(failedEntries)) return { success: false };
 
-    return { success: true };
-  });
+      return { success: true };
+    },
+  );

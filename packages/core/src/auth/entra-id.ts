ddf6a23925c30961335ad114da96537b972e4b15
Declan L. Scott <declanlscott@gmail.com>
2025-04-28 00:13:08 -0500

    refactor: issuer success callback

--- packages/core/src/auth/entra-id.ts
+++ packages/core/src/auth/entra-id.ts
@@ -2,18 +2,14 @@ import { ConfidentialClientApplication } from "@azure/msal-node";
 import { Oauth2Provider } from "@openauthjs/openauth/provider/oauth2";
 import * as R from "remeda";
 import { Resource } from "sst";
-import * as v from "valibot";
 
 import { withActor } from "../actors/context";
-import { Credentials, SignatureV4 } from "../aws";
-import { withAws } from "../aws/context";
 import { useTransaction } from "../drizzle/context";
 import { SharedErrors } from "../errors/shared";
 import { Graph } from "../graph";
 import { Tenants } from "../tenants";
 import { Users } from "../users";
 import { Constants } from "../utils/constants";
-import { fn } from "../utils/shared";
 
 import type { Oauth2WrappedConfig } from "@openauthjs/openauth/provider/oauth2";
 import type { UserSubjectProperties } from "./shared";
@@ -33,128 +29,103 @@ export namespace EntraId {
       },
     });
 
-  export const handleUser = fn(
-    v.looseObject({ aud: v.string(), tid: v.string() }),
-    async ({ aud, tid }): Promise<UserSubjectProperties> => {
-      if (aud !== Resource.Oauth2.entraId.clientId)
-        throw new Error("invalid audience");
+  export async function handleUser(
+    providerId: string,
+  ): Promise<UserSubjectProperties> {
+    const { id, userPrincipalName, preferredName, mail } = await Graph.me();
+    if (!id || !userPrincipalName || !preferredName || !mail)
+      throw new Error("missing user info");
 
-      const { id, userPrincipalName, preferredName, mail } = await Graph.me();
-      if (!id || !userPrincipalName || !preferredName || !mail)
-        throw new Error("missing user info");
+    const tenant = await Tenants.byOauth2Provider(
+      Constants.ENTRA_ID,
+      providerId,
+    );
+    if (!tenant) throw new Error("tenant not found");
 
-      const tenant = await Tenants.byOauth2Provider(Constants.ENTRA_ID, tid);
-      if (!tenant) throw new Error("tenant not found");
+    return withActor(
+      () => ({
+        kind: Constants.ACTOR_KINDS.SYSTEM,
+        properties: { tenantId: tenant.id },
+      }),
+      async () =>
+        useTransaction(async () => {
+          let user = await Users.byOauth2(id, providerId).then(R.first());
 
-      return withActor(
-        () => ({
-          kind: Constants.ACTOR_KINDS.SYSTEM,
-          properties: { tenantId: tenant.id },
+          switch (tenant.status) {
+            case "setup": {
+              if (!user) {
+                user = await Users.put([
+                  {
+                    origin: "internal",
+                    username: userPrincipalName,
+                    oauth2UserId: id,
+                    oauth2ProviderId: providerId,
+                    role: "administrator",
+                    name: preferredName,
+                    email: mail,
+                    tenantId: tenant.id,
+                  },
+                ]).then(R.first());
+                if (!user) throw new Error("failed to create admin user");
+
+                return {
+                  id: user.id,
+                  tenantId: tenant.id,
+                };
+              }
+
+              if (
+                user.username !== userPrincipalName ||
+                user.name !== preferredName ||
+                user.email !== mail
+              )
+                await Users.updateOne({
+                  id: user.id,
+                  username: userPrincipalName,
+                  name: preferredName,
+                  email: mail,
+                });
+
+              return {
+                id: user.id,
+                tenantId: tenant.id,
+              };
+            }
+            case "active": {
+              if (!user) throw new Error("user not found");
+
+              if (
+                user.username !== userPrincipalName ||
+                user.name !== preferredName ||
+                user.email !== mail
+              )
+                await Users.updateOne({
+                  id: user.id,
+                  username: userPrincipalName,
+                  name: preferredName,
+                  email: mail,
+                });
+
+              return {
+                id: user.id,
+                tenantId: tenant.id,
+              };
+            }
+            case "suspended":
+              throw new Error("tenant suspended");
+            default:
+              throw new SharedErrors.NonExhaustiveValue(tenant.status);
+          }
         }),
-        async () =>
-          withAws(
-            () => ({
-              sigv4: {
-                signers: {
-                  "execute-api": SignatureV4.buildSigner({
-                    region: Resource.Aws.region,
-                    service: "execute-api",
-                  }),
-                  appsync: SignatureV4.buildSigner({
-                    region: Resource.Aws.region,
-                    service: "appsync",
-                    credentials: Credentials.fromRoleChain({
-                      RoleArn: Credentials.buildRoleArn(
-                        Resource.Aws.tenant.roles.realtimePublisher
-                          .nameTemplate,
-                      ),
-                      RoleSessionName: "EntraIdUserHandler",
-                    }),
-                  }),
-                },
-              },
-            }),
-            async () =>
-              useTransaction(async () => {
-                let user = await Users.byOauth2(id, tid).then(R.first());
+    );
+  }
 
-                switch (tenant.status) {
-                  case "setup": {
-                    if (!user) {
-                      user = await Users.put([
-                        {
-                          origin: "internal",
-                          username: userPrincipalName,
-                          oauth2UserId: id,
-                          oauth2ProviderId: tid,
-                          role: "administrator",
-                          name: preferredName,
-                          email: mail,
-                          tenantId: tenant.id,
-                        },
-                      ]).then(R.first());
-                      if (!user) throw new Error("failed to create admin user");
-
-                      return {
-                        id: user.id,
-                        tenantId: tenant.id,
-                      };
-                    }
-
-                    if (
-                      user.username !== userPrincipalName ||
-                      user.name !== preferredName ||
-                      user.email !== mail
-                    )
-                      await Users.updateOne({
-                        id: user.id,
-                        username: userPrincipalName,
-                        name: preferredName,
-                        email: mail,
-                      });
-
-                    return {
-                      id: user.id,
-                      tenantId: tenant.id,
-                    };
-                  }
-                  case "active": {
-                    if (!user) throw new Error("user not found");
-
-                    if (
-                      user.username !== userPrincipalName ||
-                      user.name !== preferredName ||
-                      user.email !== mail
-                    )
-                      await Users.updateOne({
-                        id: user.id,
-                        username: userPrincipalName,
-                        name: preferredName,
-                        email: mail,
-                      });
-
-                    return {
-                      id: user.id,
-                      tenantId: tenant.id,
-                    };
-                  }
-                  case "suspended":
-                    throw new Error("tenant suspended");
-                  default:
-                    throw new SharedErrors.NonExhaustiveValue(tenant.status);
-                }
-              }),
-          ),
-      );
-    },
-  );
-
-  export async function applicationAccessToken(tenantId: string) {
+  export async function applicationAccessToken(providerId: string) {
     const cca = new ConfidentialClientApplication({
       auth: {
         clientId: Resource.Oauth2.entraId.clientId,
         clientSecret: Resource.Oauth2.entraId.clientSecret,
-        authority: `https://login.microsoftonline.com/${tenantId}`,
+        authority: `https://login.microsoftonline.com/${providerId}`,
       },
     });
 
--- packages/functions/node/src/issuer.ts
+++ packages/functions/node/src/issuer.ts
@@ -2,12 +2,15 @@ import { issuer } from "@openauthjs/openauth";
 import { decodeJWT } from "@oslojs/jwt";
 import { EntraId } from "@printdesk/core/auth/entra-id";
 import { subjects } from "@printdesk/core/auth/subjects";
+import { Credentials, SignatureV4 } from "@printdesk/core/aws";
+import { withAws } from "@printdesk/core/aws/context";
 import { SharedErrors } from "@printdesk/core/errors/shared";
 import { Graph } from "@printdesk/core/graph";
 import { withGraph } from "@printdesk/core/graph/context";
 import { Constants } from "@printdesk/core/utils/constants";
 import { handle } from "hono/aws-lambda";
 import { Resource } from "sst";
+import * as v from "valibot";
 
 const app = issuer({
   subjects,
@@ -19,27 +22,55 @@ const app = issuer({
       scopes: [...Constants.ENTRA_ID_OAUTH_SCOPES],
     }),
   },
-  success: async (ctx, value) => {
-    switch (value.provider) {
-      case Constants.ENTRA_ID: {
-        return withGraph(
-          () =>
-            Graph.Client.init({
-              authProvider: (done) => done(null, value.tokenset.access),
+  success: async (ctx, value) =>
+    withAws(
+      () => ({
+        sigv4: {
+          signers: {
+            "execute-api": SignatureV4.buildSigner({
+              region: Resource.Aws.region,
+              service: "execute-api",
             }),
-          async () => {
-            const properties = await EntraId.handleUser(
+            appsync: SignatureV4.buildSigner({
+              region: Resource.Aws.region,
+              service: "appsync",
+              credentials: Credentials.fromRoleChain({
+                RoleArn: Credentials.buildRoleArn(
+                  Resource.Aws.tenant.roles.realtimePublisher.nameTemplate,
+                ),
+                RoleSessionName: "Issuer",
+              }),
+            }),
+          },
+        },
+      }),
+      async () => {
+        switch (value.provider) {
+          case Constants.ENTRA_ID: {
+            const { aud, tid } = v.parse(
+              v.looseObject({ aud: v.string(), tid: v.string() }),
               decodeJWT(value.tokenset.access),
             );
 
-            return ctx.subject(Constants.SUBJECT_KINDS.USER, properties);
-          },
-        );
-      }
-      default:
-        throw new SharedErrors.NonExhaustiveValue(value.provider);
-    }
-  },
+            if (aud !== value.clientID) throw new Error("invalid audience");
+
+            return withGraph(
+              () =>
+                Graph.Client.init({
+                  authProvider: (done) => done(null, value.tokenset.access),
+                }),
+              async () => {
+                const properties = await EntraId.handleUser(tid);
+
+                return ctx.subject(Constants.SUBJECT_KINDS.USER, properties);
+              },
+            );
+          }
+          default:
+            throw new SharedErrors.NonExhaustiveValue(value.provider);
+        }
+      },
+    ),
 });
 
 export const handler = handle(app);

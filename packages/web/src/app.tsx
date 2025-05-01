0e3a67b271be653bd5103ff4ac3ae1bc6c0cea62
Declan L. Scott <declanlscott@gmail.com>
2025-05-01 15:00:32 -0500

    refactor: app component

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -7,26 +7,18 @@ import { RouterProvider } from "~/lib/contexts/router";
 import { AuthStoreApiProvider } from "~/lib/contexts/stores/auth/provider";
 import { TrpcProvider } from "~/lib/contexts/trpc/provider";
 
-export function App() {
-  // Hide the initial loading indicator
-  // Router will handle the loading indicator afterwards with `defaultPendingComponent`
-  document
-    .getElementById("initial-app-loading-indicator")
-    ?.style.setProperty("display", "none");
+export const App = () => (
+  <ResourceProvider>
+    <QueryClientProvider>
+      <AuthStoreApiProvider>
+        <TrpcProvider>
+          <ReplicacheProvider>
+            <RouterProvider />
 
-  return (
-    <ResourceProvider>
-      <QueryClientProvider>
-        <AuthStoreApiProvider>
-          <TrpcProvider>
-            <ReplicacheProvider>
-              <RouterProvider />
-
-              <Toaster richColors />
-            </ReplicacheProvider>
-          </TrpcProvider>
-        </AuthStoreApiProvider>
-      </QueryClientProvider>
-    </ResourceProvider>
-  );
-}
+            <Toaster richColors />
+          </ReplicacheProvider>
+        </TrpcProvider>
+      </AuthStoreApiProvider>
+    </QueryClientProvider>
+  </ResourceProvider>
+);

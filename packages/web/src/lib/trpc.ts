fefd0bbea012594c98214fd99510a20d9b63742e
Declan L. Scott <declanlscott@gmail.com>
2025-05-01 20:56:05 -0500

    feat: create trpc client function

---
+++ packages/web/src/lib/trpc.ts
@@ -0,0 +1,35 @@
+import { Constants } from "@printdesk/core/utils/constants";
+import {
+  createTRPCClient,
+  httpBatchLink,
+  httpLink,
+  splitLink,
+} from "@trpc/client";
+
+import type { NanoId } from "@printdesk/core/utils/shared";
+import type { StartsWith } from "@printdesk/core/utils/types";
+import type { TrpcRouter } from "@printdesk/functions/api/trpc/routers";
+
+export function createTrpcClient<TBearerToken extends string>(
+  url: URL,
+  bearerToken?: StartsWith<"Bearer ", TBearerToken>,
+  tenantId?: NanoId,
+) {
+  const linkOptions = {
+    url,
+    headers: {
+      ...(bearerToken ? { Authorization: bearerToken } : {}),
+      ...(tenantId ? { [Constants.HEADER_NAMES.TENANT_ID]: tenantId } : {}),
+    },
+  };
+
+  return createTRPCClient<TrpcRouter>({
+    links: [
+      splitLink({
+        condition: (operation) => Boolean(operation.context.skipBatch),
+        true: httpLink(linkOptions),
+        false: httpBatchLink(linkOptions),
+      }),
+    ],
+  });
+}

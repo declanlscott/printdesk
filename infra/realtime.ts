9e104616512010dbe8601956c2ef8a90bb4b9220
Declan L. Scott <declanlscott@gmail.com>
2025-05-26 23:06:45 -0500

    feat(infra): appsync events custom domain

--- infra/realtime.ts
+++ infra/realtime.ts
@@ -1,4 +1,6 @@
+import { useProvider } from "~/.sst/platform/src/components/aws/helpers/provider";
 import * as custom from "./custom";
+import { domains } from "./dns";
 
 export const appsyncEventApi = new custom.aws.Appsync.EventApi(
   "AppsyncEventApi",
@@ -18,4 +20,34 @@ export const eventsChannelNamespace = new custom.aws.Appsync.ChannelNamespace(
     apiId: appsyncEventApi.apiId,
     name: "events",
   },
+);
+
+export const certificate = new sst.aws.DnsValidatedCertificate(
+  "RealtimeCertificate",
+  {
+    domainName: domains.properties.realtime,
+    dns: sst.cloudflare.dns(),
+  },
+  { provider: useProvider("us-east-1") },
+);
+
+export const domainName = new aws.appsync.DomainName("RealtimeDomainName", {
+  domainName: domains.properties.realtime,
+  certificateArn: certificate.arn,
+});
+
+export const domainNameApiAssociation =
+  new aws.appsync.DomainNameApiAssociation("RealtimeDomainNameApiAssociation", {
+    apiId: appsyncEventApi.apiId,
+    domainName: domainName.domainName,
+  });
+
+export const realtimeAlias = sst.cloudflare.dns().createAlias(
+  "Realtime",
+  {
+    name: domainName.domainName,
+    aliasName: domainName.appsyncDomainName,
+    aliasZone: domainName.hostedZoneId,
+  },
+  {},
 );

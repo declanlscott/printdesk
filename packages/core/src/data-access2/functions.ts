e169b42e0ca2460e54ccd30f7dac7654c21ab59e
Declan L. Scott <declanlscott@gmail.com>
2025-08-10 20:22:53 -0500

    refactor(core): data access policies service

--- packages/core/src/data-access2/functions.ts
+++ packages/core/src/data-access2/functions.ts
@@ -25,26 +25,103 @@ export namespace DataAccessFunctions {
   export class Policies extends Effect.Service<Policies>()(
     "@printdesk/core/data-access/Policies",
     {
-      accessors: true,
-      succeed: {
-        functions: new DataAccess.Functions()
-          .add(BillingAccountsContract.hasActiveManagerAuthorization)
-          .add(BillingAccountsContract.hasActiveCustomerAuthorization)
-          .add(BillingAccountsContract.hasActiveAuthorization)
-          .add(CommentsContract.isAuthor)
-          .add(OrdersContract.isCustomer)
-          .add(OrdersContract.isManager)
-          .add(OrdersContract.isCustomerOrManager)
-          .add(OrdersContract.hasActiveManagerAuthorization)
-          .add(OrdersContract.canEdit)
-          .add(OrdersContract.canApprove)
-          .add(OrdersContract.canTransition)
-          .add(OrdersContract.canDelete)
-          .add(TenantsContract.isSubdomainAvailable)
-          .add(LicensesContract.isAvailable)
-          .add(UsersContract.isSelf)
-          .done(),
-      },
+      effect: Effect.gen(function* () {
+        const functions = yield* Effect.succeed(
+          new DataAccess.Functions()
+            .add(BillingAccountsContract.hasActiveManagerAuthorization)
+            .add(BillingAccountsContract.hasActiveCustomerAuthorization)
+            .add(BillingAccountsContract.hasActiveAuthorization)
+            .add(CommentsContract.isAuthor)
+            .add(OrdersContract.isCustomer)
+            .add(OrdersContract.isManager)
+            .add(OrdersContract.isCustomerOrManager)
+            .add(OrdersContract.hasActiveManagerAuthorization)
+            .add(OrdersContract.canEdit)
+            .add(OrdersContract.canApprove)
+            .add(OrdersContract.canTransition)
+            .add(OrdersContract.canDelete)
+            .add(TenantsContract.isSubdomainAvailable)
+            .add(LicensesContract.isAvailable)
+            .add(UsersContract.isSelf)
+            .done(),
+        ).pipe(Effect.cached);
+        type PolicyRecord = Effect.Effect.Success<
+          typeof functions
+        >["RecordType"];
+
+        const InvocationSchema = yield* functions.pipe(
+          Effect.map(({ map }) => map),
+          Effect.map(HashMap.values),
+          Effect.map(
+            Iterable.map(
+              (fn) =>
+                Schema.Struct({
+                  name: Schema.Literal(fn.name),
+                  args: fn.Args,
+                }) as {
+                  [TName in keyof PolicyRecord]: Schema.Struct<{
+                    name: Schema.Literal<[TName]>;
+                    args: PolicyRecord[TName]["Args"];
+                  }>;
+                }[keyof PolicyRecord],
+            ),
+          ),
+          Effect.map((members) => Schema.Union(...members)),
+          Effect.cached,
+        );
+
+        const dispatch = <
+          TName extends keyof PolicyRecord,
+          TPolicyError,
+          TPolicyContext,
+          TMakePolicyError,
+          TMakePolicyContext,
+        >(
+          makePolicy: DataAccess.MakePolicyShape<
+            TName,
+            PolicyRecord[TName]["Args"],
+            TPolicyError,
+            TPolicyContext,
+            TMakePolicyError,
+            TMakePolicyContext
+          >,
+          args:
+            | { encoded: Schema.Schema.Encoded<PolicyRecord[TName]["Args"]> }
+            | { decoded: Schema.Schema.Type<PolicyRecord[TName]["Args"]> },
+        ) =>
+          Effect.gen(function* () {
+            const { map } = yield* functions;
+
+            const { name, make } = yield* makePolicy;
+
+            const { Args } = yield* map.pipe(HashMap.get(name), Effect.orDie);
+
+            const decodedArgs = yield* "encoded" in args
+              ? Effect.succeed(args.encoded).pipe(
+                  Effect.flatMap(
+                    Schema.decode<
+                      Schema.Schema.Type<PolicyRecord[TName]["Args"]>,
+                      Schema.Schema.Encoded<PolicyRecord[TName]["Args"]>,
+                      never
+                      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                    >(Args),
+                  ),
+                )
+              : Effect.succeed(args.decoded).pipe(
+                  Effect.flatMap(
+                    Schema.decode<
+                      Schema.Schema.Type<PolicyRecord[TName]["Args"]>,
+                      Schema.Schema.Type<PolicyRecord[TName]["Args"]>,
+                      never
+                    >(Schema.typeSchema(Args)),
+                  ),
+                );
+
+            return yield* make(decodedArgs);
+          });
+
+        return { functions, InvocationSchema, dispatch } as const;
+      }),
     },
   ) {}
 
@@ -54,7 +131,7 @@ export namespace DataAccessFunctions {
       effect: Effect.gen(function* () {
         const session = yield* Auth.Session;
 
-        const functions = Effect.succeed(
+        const functions = yield* Effect.succeed(
           new DataAccess.Functions()
             .add(AnnouncementsContract.create)
             .add(AnnouncementsContract.update)
@@ -86,10 +163,33 @@ export namespace DataAccessFunctions {
             .add(UsersContract.restore)
             .add(WorkflowsContract.set)
             .done(),
-        );
+        ).pipe(Effect.cached);
         type MutationRecord = Effect.Effect.Success<
           typeof functions
         >["RecordType"];
+
+        const ReplicacheSchema = yield* functions.pipe(
+          Effect.map(({ map }) => map),
+          Effect.map(HashMap.values),
+          Effect.map(
+            Iterable.map((fn) =>
+              Schema.extend(
+                ReplicacheContract.MutationV1.omit("name", "args"),
+                Schema.Struct({
+                  name: Schema.Literal(fn.name),
+                  args: fn.Args,
+                }) as {
+                  [TName in keyof MutationRecord]: Schema.Struct<{
+                    name: Schema.Literal<[TName]>;
+                    args: MutationRecord[TName]["Args"];
+                  }>;
+                }[keyof MutationRecord],
+              ),
+            ),
+          ),
+          Effect.map((members) => Schema.Union(...members)),
+          Effect.cached,
+        );
 
         const dispatch = <
           TName extends keyof MutationRecord,
@@ -162,29 +262,7 @@ export namespace DataAccessFunctions {
             );
           });
 
-        const ReplicacheSchema = functions.pipe(
-          Effect.map(({ map }) => map),
-          Effect.map(HashMap.values),
-          Effect.map(
-            Iterable.map((fn) =>
-              Schema.extend(
-                ReplicacheContract.MutationV1.omit("name", "args"),
-                Schema.Struct({
-                  name: Schema.Literal(fn.name),
-                  args: fn.Args,
-                }) as {
-                  [TName in keyof MutationRecord]: Schema.Struct<{
-                    name: Schema.Literal<[TName]>;
-                    args: MutationRecord[TName]["Args"];
-                  }>;
-                }[keyof MutationRecord],
-              ),
-            ),
-          ),
-          Effect.map((members) => Schema.Union(...members)),
-        );
-
-        return { functions, dispatch, ReplicacheSchema };
+        return { functions, ReplicacheSchema, dispatch };
       }),
     },
   ) {}

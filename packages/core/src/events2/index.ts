c983e80148fad4cd1a814141ae4b1681b54df835
Declan L. Scott <declanlscott@gmail.com>
2025-09-29 23:56:15 -0500

    feat(core): realtime module

---
+++ packages/core/src/events2/index.ts
@@ -0,0 +1,82 @@
+import { Effect, Schema } from "effect";
+
+import { DataAccessProcedures } from "../data-access2/procedures";
+import { Permissions } from "../permissions2";
+
+export namespace Events {
+  export class InfraProvisionResult extends Schema.TaggedClass<InfraProvisionResult>(
+    "InfraProvisionResult",
+  )("InfraProvisionResult", {
+    success: Schema.Boolean,
+    dispatchId: Schema.String,
+    retrying: Schema.Boolean,
+  }) {}
+
+  export class PapercutSyncResult extends Schema.TaggedClass<PapercutSyncResult>(
+    "PapercutSyncResult",
+  )("PapercutSyncResult", {
+    success: Schema.Boolean,
+    dispatchId: Schema.String,
+  }) {}
+
+  export const ReplicachePullPermission =
+    Permissions.Schemas.ReadPermission.pipe(
+      Effect.map((permission) =>
+        Schema.TaggedStruct("ReplicachePullPermission", { permission }),
+      ),
+    );
+  export type ReplicachePullPermission = Effect.Effect.Success<
+    typeof ReplicachePullPermission
+  >["Type"];
+
+  export const replicachePullPolicyTagName = "ReplicachePullPolicy" as const;
+  export const ReplicachePullPolicy = DataAccessProcedures.Policies.Policy.pipe(
+    Effect.map(
+      Schema.extend(
+        Schema.Struct({
+          _tag: Schema.tag(replicachePullPolicyTagName),
+        }),
+      ),
+    ),
+  );
+  export type ReplicachePullPolicy = Effect.Effect.Success<
+    typeof ReplicachePullPolicy
+  >["Type"];
+
+  export const makeReplicachePullPolicy = <
+    TPolicy extends Effect.Effect.Success<
+      typeof DataAccessProcedures.Policies.Policy
+    >["Type"],
+  >(
+    policy: TPolicy,
+  ) =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    ({
+      ...policy,
+      _tag: replicachePullPolicyTagName,
+    }) as const;
+
+  export const ReplicacheNotification = Effect.all([
+    ReplicachePullPermission,
+    ReplicachePullPolicy,
+  ]).pipe(
+    Effect.map((members) => Schema.Union(...members)),
+    Effect.map(Schema.Array),
+    Effect.map((data) => Schema.Struct({ clientGroupId: Schema.UUID, data })),
+  );
+  export type ReplicacheNotification = Effect.Effect.Success<
+    typeof ReplicacheNotification
+  >["Type"];
+
+  export const Event = ReplicacheNotification.pipe(
+    Effect.map((ReplicacheNotification) =>
+      Schema.Union(
+        InfraProvisionResult,
+        PapercutSyncResult,
+        ReplicacheNotification,
+      ),
+    ),
+    Effect.map((event) => Schema.parseJson(event)),
+  );
+  export type Event = Effect.Effect.Success<typeof Event>["Type"];
+}
---
+++ packages/core/src/realtime2/index.ts
@@ -0,0 +1,163 @@
+import { formatUrl } from "@aws-sdk/util-format-url";
+import { FetchHttpClient, HttpBody, HttpClient } from "@effect/platform";
+import { HttpRequest } from "@smithy/protocol-http";
+import {
+  Array,
+  Chunk,
+  Duration,
+  Effect,
+  Option,
+  Schema,
+  Stream,
+  Struct,
+} from "effect";
+
+import { Auth } from "../auth2";
+import { SignatureV4 } from "../aws2";
+import { DataAccessProcedures } from "../data-access2/procedures";
+import { Events } from "../events2";
+import { Permissions } from "../permissions2";
+import { Sst } from "../sst";
+import { Utils } from "../utils";
+
+import type { StartsWith } from "../utils/types";
+
+export namespace Realtime {
+  export class Realtime extends Effect.Service<Realtime>()(
+    "@printdesk/core/realtime/Realtime",
+    {
+      dependencies: [
+        Sst.Resource.layer,
+        FetchHttpClient.layer,
+        Permissions.Schemas.Default,
+        DataAccessProcedures.Policies.Default,
+      ],
+      effect: Effect.gen(function* () {
+        const maybeSession = yield* Effect.serviceOption(Auth.Session);
+        const dns = yield* Sst.Resource.AppsyncEventApi.pipe(
+          Effect.map(Struct.get("dns")),
+        );
+        const nameTemplate = yield* Sst.Resource.TenantDomains.pipe(
+          Effect.map(Struct.get("realtime")),
+          Effect.map(Struct.get("nameTemplate")),
+        );
+        const sigv4 = yield* SignatureV4.Signer;
+        const httpClient = yield* HttpClient.HttpClient;
+
+        const encode = yield* Events.Event.pipe(Effect.map(Schema.encode));
+
+        const url = maybeSession.pipe(
+          Option.match({
+            onSome: (session) =>
+              Utils.buildName(nameTemplate, session.tenantId),
+            onNone: () => dns.realtime,
+          }),
+          (hostname) =>
+            formatUrl(
+              new HttpRequest({
+                protocol: "wss:",
+                hostname,
+                path: "/event/realtime",
+              }),
+            ),
+          Effect.succeed,
+          Effect.withSpan("Realtime.Realtime.url"),
+        );
+
+        const getAuth = Effect.fn("Realtime.Realtime.getAuth")(
+          ({
+            expiresIn,
+            body,
+          }: {
+            expiresIn?: Duration.Duration;
+            body: string;
+          }) =>
+            maybeSession.pipe(
+              Option.match({
+                onSome: (session) =>
+                  Utils.buildName(nameTemplate, session.tenantId),
+                onNone: () => dns.http,
+              }),
+              (hostname) =>
+                new HttpRequest({
+                  method: "POST",
+                  protocol: "https:",
+                  hostname,
+                  path: "/event",
+                  headers: {
+                    accept: "application/json, text/javascript",
+                    "content-encoding": "amz-1.0",
+                    "content-type": "application/json; charset=UTF-8",
+                    host: hostname,
+                  },
+                  body,
+                }),
+              (req) =>
+                Effect.promise(() =>
+                  expiresIn
+                    ? sigv4.presign(req, {
+                        expiresIn: expiresIn.pipe(Duration.toSeconds),
+                      })
+                    : sigv4.sign(req),
+                ),
+              Effect.map(Struct.get("headers")),
+            ),
+        );
+
+        const publish = Effect.fn("Realtime.Realtime.publish")(
+          <TChannel extends string>({
+            channel,
+            events,
+          }: {
+            channel: StartsWith<"/", TChannel>;
+            events: ReadonlyArray<Events.Event>;
+          }) =>
+            maybeSession.pipe(
+              Option.match({
+                onSome: (session) =>
+                  Utils.buildName(nameTemplate, session.tenantId),
+                onNone: () => dns.http,
+              }),
+              (hostname) =>
+                Effect.all(
+                  Array.map(events, (event) => encode(event)),
+                  { concurrency: "unbounded" },
+                ).pipe(
+                  Effect.map(Stream.fromIterable),
+                  Effect.map(Stream.grouped(5)),
+                  Effect.map(Stream.map(Chunk.toArray)),
+                  Effect.flatMap(
+                    Stream.runForEach((events) =>
+                      Effect.promise(() =>
+                        sigv4.sign(
+                          new HttpRequest({
+                            method: "POST",
+                            protocol: "https:",
+                            hostname,
+                            path: "/event",
+                            headers: {
+                              "content-type": "application/json",
+                              host: hostname,
+                            },
+                            body: JSON.stringify({ channel, events }),
+                          }),
+                        ),
+                      ).pipe(
+                        Effect.flatMap((req) =>
+                          httpClient.post(formatUrl(req), {
+                            headers: req.headers,
+                            body: HttpBody.raw(req.body),
+                          }),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ),
+        );
+
+        return { url, getAuth, publish } as const;
+      }),
+    },
+  ) {}
+}

65f2e9c45a9c94c9bb629d21b4faedfa1bccec51
Declan L. Scott <declanlscott@gmail.com>
2025-09-29 23:58:13 -0500

    refactor(core): sst resource layer and runtime

--- packages/core/src/sst/index.ts
+++ packages/core/src/sst/index.ts
@@ -1,21 +1,23 @@
-import { Effect, Layer } from "effect";
-import { Resource as _Resource } from "sst";
+import { Effect, Layer, ManagedRuntime } from "effect";
+import { Resource as SstResource } from "sst";
 
 import type { Context } from "effect";
 
 export namespace Sst {
   export class Resource extends Effect.Tag("@printdesk/core/sst/Resource")<
     Sst.Resource,
-    { readonly [TKey in keyof _Resource]: Effect.Effect<_Resource[TKey]> }
+    { readonly [TKey in keyof SstResource]: Effect.Effect<SstResource[TKey]> }
   >() {
     static layer = Layer.succeed(
       this,
       new Proxy({} as Context.Tag.Service<Sst.Resource>, {
-        get: (_, key: keyof _Resource) =>
-          Effect.succeed(_Resource[key]).pipe(
+        get: (_, key: keyof SstResource) =>
+          Effect.succeed(SstResource[key]).pipe(
             Effect.withSpan(`Sst.Resource.${key}`),
           ),
       }),
     );
   }
+
+  export const runtime = ManagedRuntime.make(Sst.Resource.layer);
 }

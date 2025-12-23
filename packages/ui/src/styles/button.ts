8433eac41a1fd4970238896e4b7c90a80257c577
Declan L. Scott <declanlscott@gmail.com>
2025-12-23 12:08:24 -0600

    feat(ui): squircle corners

--- packages/ui/src/styles/button.ts
+++ packages/ui/src/styles/button.ts
@@ -6,7 +6,7 @@ import type { VariantProps } from "tailwind-variants";
 
 export const button = tv({
   extend: focusRing,
-  base: "relative inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 cursor-pointer pressed:scale-95",
+  base: "relative inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-all [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 cursor-pointer pressed:scale-95 corner-squircle",
   variants: {
     variant: {
       default:
@@ -23,8 +23,8 @@ export const button = tv({
     },
     size: {
       default: "h-9 px-4 py-2 has-[>svg]:px-3",
-      sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-      lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+      sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+      lg: "h-10 px-6 has-[>svg]:px-4",
       icon: "size-9",
       "icon-sm": "size-8",
       "icon-lg": "size-10",

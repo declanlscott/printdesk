996d0f80cc34d777e7f7a2d2074b414b9dd6d9d0
Declan L. Scott <declanlscott@gmail.com>
2026-01-15 15:04:04 -0600

    refactor(functions): realtime http api group

--- packages/functions/src/api/groups/realtime.ts
+++ packages/functions/src/api/groups/realtime.ts
@@ -5,19 +5,26 @@ import { Realtime } from "@printdesk/core/realtime";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 
+import { actorLayer } from "../middleware/actor";
+import { realtimeSubscriberCredentialsIdentityLayer } from "../middleware/aws";
+
 export const realtimeLayer = HttpApiBuilder.group(
   ApiContract.Application,
   "realtime",
   (handlers) =>
     handlers
-      .handle("getUrl", () =>
-        Realtime.Realtime.url.pipe(
-          Effect.mapError(() => new HttpApiError.InternalServerError()),
-        ),
-      )
       .handle("getAuthorization", ({ payload }) =>
         Realtime.Realtime.getAuthorization(payload.channel).pipe(
           Effect.mapError(() => new HttpApiError.InternalServerError()),
         ),
+      )
+      .handle("getUrl", () =>
+        Realtime.Realtime.url.pipe(
+          Effect.mapError(() => new HttpApiError.InternalServerError()),
+        ),
       ),
-).pipe(Layer.provide(Realtime.Realtime.Default));
+).pipe(
+  Layer.provide(Realtime.Realtime.Default),
+  Layer.provide(realtimeSubscriberCredentialsIdentityLayer),
+  Layer.provide(actorLayer),
+);

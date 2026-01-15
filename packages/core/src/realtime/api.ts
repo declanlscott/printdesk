31fc2c95e250467eb4eddbd32e16251509e6bc01
Declan L. Scott <declanlscott@gmail.com>
2026-01-15 14:58:46 -0600

    refactor(core): realtime http api group

--- packages/core/src/realtime/api.ts
+++ packages/core/src/realtime/api.ts
@@ -2,13 +2,11 @@ import * as HttpApiEndpoint from "@effect/platform/HttpApiEndpoint";
 import * as HttpApiError from "@effect/platform/HttpApiError";
 import * as HttpApiGroup from "@effect/platform/HttpApiGroup";
 
+import { ActorsApi } from "../actors/api";
+import { CredentialsApi } from "../aws/api";
 import { RealtimeContract } from "./contract";
 
 export namespace RealtimeApi {
-  export const getUrl = HttpApiEndpoint.get("getUrl", "/url")
-    .addSuccess(RealtimeContract.Url)
-    .addError(HttpApiError.InternalServerError);
-
   export const getAuthorization = HttpApiEndpoint.get(
     "getAuthorization",
     "/authorization",
@@ -17,7 +15,13 @@ export namespace RealtimeApi {
     .addSuccess(RealtimeContract.Authorization)
     .addError(HttpApiError.InternalServerError);
 
+  export const getUrl = HttpApiEndpoint.get("getUrl", "/url")
+    .addSuccess(RealtimeContract.Url)
+    .addError(HttpApiError.InternalServerError);
+
   export class Group extends HttpApiGroup.make("realtime")
+    .add(getAuthorization)
+    .middlewareEndpoints(CredentialsApi.Identity)
     .add(getUrl)
-    .add(getAuthorization) {}
+    .middleware(ActorsApi.Actor) {}
 }

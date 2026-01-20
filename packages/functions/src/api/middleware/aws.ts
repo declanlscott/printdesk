1d736f0e17e52d1825956b9234dc9fbe587be38c
Declan L. Scott <declanlscott@gmail.com>
2026-01-20 17:29:55 -0600

    feat(functions): aws credential identity http api middleware

--- packages/functions/src/api/middleware/aws.ts
+++ packages/functions/src/api/middleware/aws.ts
@@ -1,5 +1,4 @@
 import { fromTemporaryCredentials } from "@aws-sdk/credential-providers";
-import * as HttpApiError from "@effect/platform/HttpApiError";
 import { Actors } from "@printdesk/core/actors";
 import { Credentials } from "@printdesk/core/aws";
 import { CredentialsApi } from "@printdesk/core/aws/api";
@@ -15,14 +14,15 @@ import * as Struct from "effect/Struct";
 import type { FromTemporaryCredentialsOptions } from "@aws-sdk/credential-providers";
 import type { ActorsContract } from "@printdesk/core/actors/contract";
 
+// TODO: `Context.unsafeGet` is a workaround because `HttpApiMiddleware` doesn't
+// support requirements yet, but this should be fixed in Effect v4.0:
+// https://github.com/Effect-TS/effect/pull/5443#issuecomment-3246785154
+
 export const realtimeSubscriberCredentialsIdentityLayer = Effect.gen(
   function* () {
     const resource = yield* Sst.Resource;
 
     return CredentialsApi.Identity.of(
-      // NOTE: `Context.unsafeGet` is a workaround because `HttpApiMiddleware` doesn't
-      // support requirements yet, but this should be fixed in Effect v4.0:
-      // https://github.com/Effect-TS/effect/pull/5443#issuecomment-3246785154
       Effect.context<never>().pipe(
         Effect.map(Context.unsafeGet(Actors.Actor)),
         Effect.map(Struct.get("properties")),
@@ -54,11 +54,40 @@ export const realtimeSubscriberCredentialsIdentityLayer = Effect.gen(
           ),
         ),
         Effect.map((params) => () => fromTemporaryCredentials({ params })),
-        Effect.flatMap((provider) =>
-          Credentials.Identity.fromProvider(provider),
-        ),
-        Effect.mapError(() => new HttpApiError.InternalServerError()),
+        Effect.flatMap(Credentials.Identity.fromProvider),
       ),
     );
   },
-).pipe(Layer.effect(CredentialsApi.Identity));
+).pipe(
+  Layer.effect(CredentialsApi.Identity),
+  Layer.provide(Sst.Resource.layer),
+);
+
+export const tenantRealtimePublisherCredentialsIdentityLayer = Effect.gen(
+  function* () {
+    const resource = yield* Sst.Resource;
+
+    return CredentialsApi.Identity.of(
+      Effect.context<never>().pipe(
+        Effect.map(Context.unsafeGet(Actors.Actor)),
+        Effect.flatMap(Struct.get("assertPrivate")),
+        Effect.map(
+          ({ tenantId }) =>
+            ({
+              RoleArn: tenantTemplate(
+                resource.TenantRoles.pipe(Redacted.value).realtimePublisher
+                  .nameTemplate,
+                tenantId,
+              ),
+              RoleSessionName: "TenantRealtimePublisher",
+            }) satisfies FromTemporaryCredentialsOptions["params"],
+        ),
+        Effect.map((params) => () => fromTemporaryCredentials({ params })),
+        Effect.flatMap(Credentials.Identity.fromProvider),
+      ),
+    );
+  },
+).pipe(
+  Layer.effect(CredentialsApi.Identity),
+  Layer.provide(Sst.Resource.layer),
+);

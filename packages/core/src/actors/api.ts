a29aa15d7cbe0a9f9414d7bec19dd3daf05eb29e
Declan L. Scott <declanlscott@gmail.com>
2026-01-20 17:25:34 -0600

    refactor(core): actor http api middleware

--- packages/core/src/actors/api.ts
+++ packages/core/src/actors/api.ts
@@ -1,11 +1,26 @@
 import * as HttpApiError from "@effect/platform/HttpApiError";
 import * as HttpApiMiddleware from "@effect/platform/HttpApiMiddleware";
+import * as Schema from "effect/Schema";
 
 import { Actors } from ".";
+import { CryptoContract } from "../auth/contracts";
+import { Oauth } from "../auth/oauth";
+import { DatabaseContract } from "../database/contract";
 
 export namespace ActorsApi {
   export class Actor extends HttpApiMiddleware.Tag<Actor>()("Actor", {
-    failure: HttpApiError.Unauthorized,
+    failure: Schema.Union(
+      HttpApiError.HttpApiDecodeError,
+      HttpApiError.Unauthorized,
+      DatabaseContract.TransactionError,
+      CryptoContract.KeyDerivationError,
+      CryptoContract.KeyBufferError,
+      CryptoContract.InvalidSecretError,
+      CryptoContract.KeyVerificationError,
+      Oauth.ClientError,
+      Oauth.InvalidRefreshTokenError,
+      Oauth.InvalidAccessTokenError,
+    ),
     provides: Actors.Actor,
   }) {}
 }

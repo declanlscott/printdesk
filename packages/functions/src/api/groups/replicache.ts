c78b778bd39f47f0ddc6e2a953fdc808c211d0cb
Declan L. Scott <declanlscott@gmail.com>
2026-01-27 13:26:28 -0600

    refactor(functions): replicache http api handlers

--- packages/functions/src/api/groups/replicache.ts
+++ packages/functions/src/api/groups/replicache.ts
@@ -1,9 +1,15 @@
 import * as HttpApiBuilder from "@effect/platform/HttpApiBuilder";
+import * as HttpApiError from "@effect/platform/HttpApiError";
 import { ApiContract } from "@printdesk/core/api/contract";
+import {
+  ReplicachePullerContract,
+  ReplicachePusherContract,
+} from "@printdesk/core/replicache/contracts";
 import { ReplicachePuller } from "@printdesk/core/replicache/pull";
 import { ReplicachePusher } from "@printdesk/core/replicache/push";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
+import * as Schema from "effect/Schema";
 
 import { actorLayer } from "../middleware/actor";
 import { tenantRealtimePublisherCredentialsIdentityLayer } from "../middleware/aws";
@@ -13,20 +19,40 @@ export const replicacheLayer = HttpApiBuilder.group(
   "replicache",
   (handlers) =>
     handlers
-      .handle("pull", ({ payload: request }) =>
-        ReplicachePuller.pull(request).pipe(
+      .handle("pull", ({ payload }) =>
+        Effect.succeed(payload).pipe(
+          Effect.flatMap(Schema.decode(ReplicachePullerContract.Request)),
+          Effect.catchTag("ParseError", (e) =>
+            HttpApiError.HttpApiDecodeError.refailParseError(e),
+          ),
+          Effect.flatMap(ReplicachePuller.pull),
           Effect.catchTags({
             ClientStateNotFoundError: (e) => Effect.succeed(e.response),
             VersionNotSupportedError: (e) => Effect.succeed(e.response),
           }),
+          Effect.flatMap(Schema.encode(ReplicachePullerContract.Response)),
+          Effect.catchTag(
+            "ParseError",
+            () => new HttpApiError.InternalServerError(),
+          ),
         ),
       )
-      .handle("push", ({ payload: request }) =>
-        ReplicachePusher.push(request).pipe(
+      .handle("push", ({ payload }) =>
+        Effect.succeed(payload).pipe(
+          Effect.flatMap(Schema.decode(ReplicachePusherContract.Request)),
+          Effect.catchTag("ParseError", (e) =>
+            HttpApiError.HttpApiDecodeError.refailParseError(e),
+          ),
+          Effect.flatMap(ReplicachePusher.push),
           Effect.catchTags({
             ClientStateNotFoundError: (e) => Effect.succeed(e.response),
             VersionNotSupportedError: (e) => Effect.succeed(e.response),
           }),
+          Effect.flatMap(Schema.encode(ReplicachePusherContract.Response)),
+          Effect.catchTag(
+            "ParseError",
+            () => new HttpApiError.InternalServerError(),
+          ),
         ),
       ),
 ).pipe(

52a2305aef13c194c864473f6aa3c0b49d1429e5
Declan L. Scott <declanlscott@gmail.com>
2026-05-30 22:41:22 -0500

    chore(core): remove old events module

--- packages/typescript/core/src/events/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Schema from "effect/Schema";
-
-import { PapercutContract } from "../papercut/contract";
-import { ReplicacheContract } from "../replicache/contracts";
-
-export namespace Events {
-  export const Event = Schema.Union([PapercutContract.SyncResult, ReplicacheContract.Notification]);
-  export type Event = typeof Event.Type;
-}

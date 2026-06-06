cc44545c6b9df12414a58050bf70a1bb9153099a
Declan L. Scott <declanlscott@gmail.com>
2026-06-05 23:51:59 -0500

    refactor(web): network atoms

--- packages/typescript/web/src/lib/network.ts
+++ packages/typescript/web/src/lib/network.ts
@@ -4,14 +4,8 @@ import * as Stream from "effect/Stream";
 import * as Struct from "effect/Struct";
 import * as Atom from "effect/unstable/reactivity/Atom";
 
-export const networkMonitorAtom = Atom.make(
-  NetworkMonitor.make({
-    initialOnline: globalThis.window.navigator.onLine,
-    addEventListener: globalThis.window.addEventListener,
-    removeEventListener: globalThis.window.removeEventListener,
-  }),
-).pipe(Atom.keepAlive);
+export const networkMonitorAtom = Atom.make(NetworkMonitor.make).pipe(Atom.keepAlive);
 
 export const onlineAtom = Atom.make((get) =>
-  networkMonitorAtom.pipe(get.result, Effect.map(Struct.get("onlineChanges")), Stream.unwrap),
+  get.resultOnce(networkMonitorAtom).pipe(Effect.map(Struct.get("onlineChanges")), Stream.unwrap),
 );

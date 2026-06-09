e9de0ebdc22a94c1436e37ca66506708ebcad019
Declan L. Scott <declanlscott@gmail.com>
2026-06-09 12:22:30 -0500

    feat(api): update cookies in actor middleware if available

--- packages/typescript/functions/api/src/middleware/actor.ts
+++ packages/typescript/functions/api/src/middleware/actor.ts
@@ -2,12 +2,17 @@ import { ActorLayerMap } from "@printdesk/core/actors";
 import { ActorsContract } from "@printdesk/core/actors/contract";
 import { Oauth } from "@printdesk/core/oauth/client";
 import { OauthContract } from "@printdesk/core/oauth/contract";
+import { Constants } from "@printdesk/core/utils/constants";
 import * as Effect from "effect/Effect";
 import * as Filter from "effect/Filter";
+import * as Option from "effect/Option";
+import * as Redacted from "effect/Redacted";
 import * as Result from "effect/Result";
+import * as Cookies from "effect/unstable/http/Cookies";
 import * as HttpRouter from "effect/unstable/http/HttpRouter";
 import * as HttpServerRequest from "effect/unstable/http/HttpServerRequest";
 import * as HttpServerRespondable from "effect/unstable/http/HttpServerRespondable";
+import * as HttpServerResponse from "effect/unstable/http/HttpServerResponse";
 
 import type { Actor } from "@printdesk/core/actors";
 
@@ -22,14 +27,40 @@ export const actorMiddleware = HttpRouter.middleware<{ provides: Actor }>()(
         Effect.mapError((error) => new OauthContract.InvalidCookiesError({ cause: error })),
       );
 
-      if ("accessToken" in cookies)
-        return yield* openauth
-          .verify(cookies.accessToken, { refresh: cookies.refreshToken })
-          .pipe(
-            Effect.flatMap((result) =>
-              httpEffect.pipe(Effect.provide(layerMap.get(result.subject.properties.actor.wrap))),
-            ),
-          );
+      if ("accessToken" in cookies) {
+        const result = yield* openauth.verify(cookies.accessToken, {
+          refresh: cookies.refreshToken,
+        });
+
+        const provided = httpEffect.pipe(
+          Effect.provide(layerMap.get(result.subject.properties.actor.wrap)),
+        );
+
+        return yield* Effect.suspend(() =>
+          result.tokens.pipe(
+            Option.match({
+              onSome: (tokens) =>
+                provided.pipe(
+                  Effect.flatMap(
+                    HttpServerResponse.setCookies([
+                      [
+                        Constants.COOKIE_NAMES.ACCESS_TOKEN,
+                        tokens.access.pipe(Redacted.value),
+                        Constants.COOKIE_OPTIONS,
+                      ],
+                      [
+                        Constants.COOKIE_NAMES.REFRESH_TOKEN,
+                        tokens.refresh.pipe(Redacted.value),
+                        Constants.COOKIE_OPTIONS,
+                      ],
+                    ]),
+                  ),
+                ),
+              onNone: () => provided,
+            }),
+          ),
+        );
+      }
 
       return yield* httpEffect.pipe(
         Effect.provide(layerMap.get(ActorsContract.PublicActor.singleton.wrap)),
@@ -39,12 +70,16 @@ export const actorMiddleware = HttpRouter.middleware<{ provides: Actor }>()(
 ).combine(
   HttpRouter.middleware<{
     handles:
+      | Cookies.CookiesError
       | OauthContract.InvalidCookiesError
       | OauthContract.InvalidAccessTokenError
       | OauthContract.InvalidRefreshTokenError
       | OauthContract.VerifyError;
   }>()((httpEffect) =>
     httpEffect.pipe(
+      Effect.catchTag("CookieError", () =>
+        HttpServerResponse.empty({ status: 500 }).pipe(Effect.succeed),
+      ),
       Effect.catchFilter(
         Filter.make((error) =>
           HttpServerRespondable.isRespondable(error) ? Result.succeed(error) : Result.fail(error),

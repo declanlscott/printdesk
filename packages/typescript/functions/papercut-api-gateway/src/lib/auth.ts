0654dc0b1fb2d91d8e00cd7ad55e1326f5b0cece
Declan L. Scott <declanlscott@gmail.com>
2026-06-09 10:22:49 -0500

    refactor(papercut-api-gateway): auth

--- packages/typescript/functions/papercut-api-gateway/src/lib/auth.ts
+++ packages/typescript/functions/papercut-api-gateway/src/lib/auth.ts
@@ -1,13 +1,15 @@
+import { ActorLayerMap } from "@printdesk/core/actors";
 import { Oauth } from "@printdesk/core/oauth/client";
 import { Constants } from "@printdesk/core/utils/constants";
+import * as Layer from "effect/Layer";
 import * as ManagedRuntime from "effect/ManagedRuntime";
 import * as Redacted from "effect/Redacted";
 
 import { lambda } from "./aws";
 import { resource } from "./sst";
 
-export const openauthRuntime = Oauth.Openauth.layer({
+export const authRuntime = Oauth.Openauth.layer({
   clientID: Constants.OPENAUTH_CLIENT_IDS.PAPERCUT_API_GATEWAY,
   fetch: lambda.fetch,
   issuer: resource.Issuer.pipe(Redacted.value).url,
-}).pipe(ManagedRuntime.make);
+}).pipe(Layer.merge(ActorLayerMap.layer), ManagedRuntime.make);
--- packages/typescript/functions/papercut-api-gateway/src/middleware/auth.ts
+++ packages/typescript/functions/papercut-api-gateway/src/middleware/auth.ts
@@ -1,6 +1,5 @@
 import { AccessControl } from "@printdesk/core/access-control";
-import { Actor } from "@printdesk/core/actors";
-import { ActorsContract } from "@printdesk/core/actors/contract";
+import { ActorLayerMap } from "@printdesk/core/actors";
 import { Oauth } from "@printdesk/core/oauth/client";
 import * as Effect from "effect/Effect";
 import * as Exit from "effect/Exit";
@@ -8,7 +7,7 @@ import * as Redacted from "effect/Redacted";
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 
-import { openauthRuntime } from "../lib/auth";
+import { authRuntime } from "../lib/auth";
 
 const accessTokenRegex = new RegExp(/^[A-Za-z0-9._~+/-]+=*/);
 
@@ -21,16 +20,13 @@ export const auth = createMiddleware(async function (c, next) {
 
   const verifyExit = await Oauth.Openauth.use((openauth) =>
     Redacted.make(accessToken).pipe(openauth.verify),
-  ).pipe(openauthRuntime.runPromiseExit);
+  ).pipe(authRuntime.runPromiseExit);
 
   if (Exit.isFailure(verifyExit)) throw new HTTPException(407);
 
   const accessExit = AccessControl.clientPermissionPolicy("papercut_api_gateway:read").pipe(
-    Effect.provideService(
-      Actor,
-      new ActorsContract.Actor({ properties: verifyExit.value.subject.properties.actor }),
-    ),
-    Effect.runSyncExit,
+    Effect.provide(ActorLayerMap.get(verifyExit.value.subject.properties.actor.wrap)),
+    authRuntime.runSyncExit,
   );
   if (Exit.isFailure(accessExit)) throw new HTTPException(403);
 

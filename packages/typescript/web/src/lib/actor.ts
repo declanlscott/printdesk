2f3d82475d24a89ff40834689109dd9172f8221f
Declan L. Scott <declanlscott@gmail.com>
2026-06-09 03:29:06 -0500

    refactor(web): actor atom

--- packages/typescript/web/src/lib/actor.ts
+++ packages/typescript/web/src/lib/actor.ts
@@ -1,6 +1,4 @@
 import { ActorsContract } from "@printdesk/core/actors/contract";
 import * as Atom from "effect/unstable/reactivity/Atom";
 
-export const actorAtom = Atom.make(
-  new ActorsContract.Actor({ properties: new ActorsContract.PublicActor() }),
-);
+export const actorAtom = Atom.make(ActorsContract.PublicActor.singleton.wrap);

2c66743ddb69517e944083e6a6e1e4e55d5cba53
Declan L. Scott <declanlscott@gmail.com>
2026-06-09 19:41:44 -0500

    refactor(api): don't set auth cookies in actor middleware
    handle this in api gateway instead

--- packages/typescript/functions/api/src/middleware/actor.ts
+++ packages/typescript/functions/api/src/middleware/actor.ts
@@ -2,11 +2,9 @@ import { ActorLayerMap } from "@printdesk/core/actors";
 import { ActorsContract } from "@printdesk/core/actors/contract";
 import { Oauth } from "@printdesk/core/oauth/client";
 import { OauthContract } from "@printdesk/core/oauth/contract";
-import { Constants } from "@printdesk/core/utils/constants";
 import * as Effect from "effect/Effect";
 import * as Filter from "effect/Filter";
-import * as Option from "effect/Option";
-import * as Redacted from "effect/Redacted";
+import * as Layer from "effect/Layer";
 import * as Result from "effect/Result";
 import * as Cookies from "effect/unstable/http/Cookies";
 import * as HttpRouter from "effect/unstable/http/HttpRouter";
@@ -27,40 +25,15 @@ export const actorMiddleware = HttpRouter.middleware<{ provides: Actor }>()(
         Effect.mapError((error) => new OauthContract.InvalidCookiesError({ cause: error })),
       );
 
-      if ("accessToken" in cookies) {
-        const result = yield* openauth.verify(cookies.accessToken, {
-          refresh: cookies.refreshToken,
-        });
-
-        const provided = httpEffect.pipe(
-          Effect.provide(layerMap.get(result.subject.properties.actor.wrap)),
-        );
-
-        return yield* Effect.suspend(() =>
-          result.tokens.pipe(
-            Option.match({
-              onSome: (tokens) =>
-                provided.pipe(
-                  Effect.flatMap(
-                    HttpServerResponse.setCookies([
-                      [
-                        Constants.COOKIE_NAMES.ACCESS_TOKEN,
-                        tokens.access.pipe(Redacted.value),
-                        Constants.COOKIE_OPTIONS,
-                      ],
-                      [
-                        Constants.COOKIE_NAMES.REFRESH_TOKEN,
-                        tokens.refresh.pipe(Redacted.value),
-                        Constants.COOKIE_OPTIONS,
-                      ],
-                    ]),
-                  ),
-                ),
-              onNone: () => provided,
-            }),
+      if ("accessToken" in cookies)
+        return yield* httpEffect.pipe(
+          Effect.provide(
+            openauth.verify(cookies.accessToken).pipe(
+              Effect.map((result) => layerMap.get(result.subject.properties.actor.wrap)),
+              Layer.unwrap,
+            ),
           ),
         );
-      }
 
       return yield* httpEffect.pipe(
         Effect.provide(layerMap.get(ActorsContract.PublicActor.singleton.wrap)),

24130a5f0d8c57e28e085a080c5aeb80bac3f1df
Declan L. Scott <declanlscott@gmail.com>
2026-06-10 02:00:46 -0500

    feat(papercut-api-gateway): proxy timeout

--- packages/typescript/functions/papercut-api-gateway/src/middleware/papercut-api.ts
+++ packages/typescript/functions/papercut-api-gateway/src/middleware/papercut-api.ts
@@ -1,5 +1,12 @@
+import * as Cause from "effect/Cause";
+import * as Duration from "effect/Duration";
+import * as Effect from "effect/Effect";
+import * as Exit from "effect/Exit";
+import * as Match from "effect/Match";
 import * as Redacted from "effect/Redacted";
+import * as Result from "effect/Result";
 import { createMiddleware } from "hono/factory";
+import { HTTPException } from "hono/http-exception";
 import { proxy } from "hono/proxy";
 
 import { resource } from "../lib/sst";
@@ -12,6 +19,27 @@ const url =
   resource.PORT.pipe(Redacted.value) +
   "/rpc/api/xmlrpc";
 
-export const papercutApi = createMiddleware(async (c) =>
-  proxy(url, { raw: c.req.raw, customFetch: resource.PAPERCUT_API.pipe(Redacted.value).fetch }),
+const customFetch = resource.PAPERCUT_API.pipe(Redacted.value).fetch;
+
+export const papercutApi = createMiddleware((c) =>
+  Effect.tryPromise((signal) => proxy(url, { raw: c.req.raw, signal, customFetch }))
+    .pipe(Effect.timeout(Duration.seconds(10)), Effect.runPromiseExit)
+    .then(
+      Exit.match({
+        onSuccess: (response) => response,
+        onFailure: (cause) => {
+          throw cause.pipe(
+            Cause.findError,
+            Result.match({
+              onSuccess: (error) =>
+                Match.valueTags(error, {
+                  TimeoutError: (error) => new HTTPException(504, { cause: error.cause }),
+                  UnknownError: (error) => new HTTPException(500, { cause: error.cause }),
+                }),
+              onFailure: (cause) => new HTTPException(500, { cause }),
+            }),
+          );
+        },
+      }),
+    ),
 );

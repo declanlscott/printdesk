57e15e0711a24dd2688afd8997dd7915078eb9a0
Declan L. Scott <declanlscott@gmail.com>
2026-06-10 01:59:10 -0500

    refactor(papercut-api-gateway): auth middleware

--- packages/typescript/functions/papercut-api-gateway/src/lib/auth.ts
+++ packages/typescript/functions/papercut-api-gateway/src/lib/auth.ts
@@ -4,6 +4,9 @@ import { Constants } from "@printdesk/core/utils/constants";
 import * as Layer from "effect/Layer";
 import * as ManagedRuntime from "effect/ManagedRuntime";
 import * as Redacted from "effect/Redacted";
+import * as Schema from "effect/Schema";
+import * as SchemaGetter from "effect/SchemaGetter";
+import * as Tuple from "effect/Tuple";
 
 import { lambda } from "./aws";
 import { resource } from "./sst";
@@ -13,3 +16,21 @@ export const authRuntime = Oauth.Openauth.layer({
   fetch: lambda.fetch,
   issuer: resource.Issuer.pipe(Redacted.value).url,
 }).pipe(Layer.merge(ActorLayerMap.layer), ManagedRuntime.make);
+
+const bearerPrefix = "Bearer " as const;
+
+export const AuthHeaders = Schema.Struct({
+  accessToken: Schema.NonEmptyString.pipe(
+    Schema.RedactedFromValue,
+    Schema.encodeTo(
+      Schema.TemplateLiteralParser([
+        bearerPrefix,
+        Schema.NonEmptyString.pipe(Schema.check(Schema.isBase64())),
+      ]),
+      {
+        encode: SchemaGetter.transform((accessToken) => Tuple.make(bearerPrefix, accessToken)),
+        decode: SchemaGetter.forbidden(() => "Not implemented"),
+      },
+    ),
+  ),
+}).pipe(Schema.encodeKeys({ accessToken: "Proxy-Authorization" }));
--- packages/typescript/functions/papercut-api-gateway/src/middleware/auth.ts
+++ packages/typescript/functions/papercut-api-gateway/src/middleware/auth.ts
@@ -1,34 +1,68 @@
 import { AccessControl } from "@printdesk/core/access-control";
 import { ActorLayerMap } from "@printdesk/core/actors";
 import { Oauth } from "@printdesk/core/oauth/client";
+import * as Cause from "effect/Cause";
 import * as Effect from "effect/Effect";
 import * as Exit from "effect/Exit";
-import * as Redacted from "effect/Redacted";
+import * as Match from "effect/match";
+import * as Result from "effect/Result";
+import * as HttpServerRequest from "effect/unstable/http/HttpServerRequest";
+import * as HttpServerRespondable from "effect/unstable/http/HttpServerRespondable";
+import * as HttpServerResponse from "effect/unstable/http/HttpServerResponse";
+import * as HttpApiError from "effect/unstable/httpapi/HttpApiError";
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 
-import { authRuntime } from "../lib/auth";
+import { AuthHeaders, authRuntime } from "../lib/auth";
 
-const accessTokenRegex = new RegExp(/^[A-Za-z0-9._~+/-]+=*/);
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 
-export const auth = createMiddleware(async function (c, next) {
-  const headerToken = c.req.header("Proxy-Authorization");
-  if (!headerToken) throw new HTTPException(407);
-
-  const accessToken = headerToken.slice("Bearer ".length);
-  if (!accessTokenRegex.test(accessToken)) throw new HTTPException(407);
-
-  const verifyExit = await Oauth.Openauth.use((openauth) =>
-    Redacted.make(accessToken).pipe(openauth.verify),
-  ).pipe(authRuntime.runPromiseExit);
-
-  if (Exit.isFailure(verifyExit)) throw new HTTPException(407);
-
-  const accessExit = AccessControl.clientPermissionPolicy("papercut_api_gateway:read").pipe(
-    Effect.provide(ActorLayerMap.get(verifyExit.value.subject.properties.actor.wrap)),
-    authRuntime.runSyncExit,
-  );
-  if (Exit.isFailure(accessExit)) throw new HTTPException(403);
-
-  return next();
-});
+export const auth = createMiddleware((c, next) =>
+  AuthHeaders.pipe(
+    (headers) =>
+      HttpApiError.HttpApiSchemaError.wrap("Headers", HttpServerRequest.schemaHeaders(headers)),
+    Effect.provideService(
+      HttpServerRequest.HttpServerRequest,
+      HttpServerRequest.fromWeb(c.req.raw),
+    ),
+    Effect.flatMap(({ accessToken }) =>
+      Oauth.Openauth.use((openauth) => openauth.verify(accessToken)),
+    ),
+    Effect.flatMap(({ subject }) =>
+      AccessControl.clientPermissionPolicy("papercut_api_gateway:read").pipe(
+        Effect.provide(ActorLayerMap.get(subject.properties.actor.wrap)),
+      ),
+    ),
+    authRuntime.runPromiseExit,
+  ).then(
+    Exit.match({
+      onSuccess: next,
+      onFailure: (cause) =>
+        cause.pipe(
+          Cause.findError,
+          Result.match({
+            onSuccess: (error) => {
+              throw error.pipe(
+                HttpServerRespondable.toResponse,
+                Effect.map(HttpServerResponse.toWeb),
+                Effect.map(
+                  (res) =>
+                    new HTTPException(
+                      Match.value(res.status).pipe(
+                        Match.when(Match.is(401), () => 407),
+                        Match.orElse((status) => status),
+                      ) as ContentfulStatusCode,
+                      { res, cause: error.cause },
+                    ),
+                ),
+                Effect.runSync,
+              );
+            },
+            onFailure: (cause) => {
+              throw new HTTPException(500, { cause });
+            },
+          }),
+        ),
+    }),
+  ),
+);

2161d251250fde14de9f230afb4f829c4e5dbe30
Declan L. Scott <declanlscott@gmail.com>
2026-06-11 01:31:01 -0500

    refactor(api-gateway): openauth runtime

--- packages/typescript/functions/api-gateway/src/lib/auth.ts
+++ packages/typescript/functions/api-gateway/src/lib/auth.ts
@@ -1,13 +1,12 @@
 import { Oauth } from "@printdesk/core/oauth/client";
 import { Constants } from "@printdesk/core/utils/constants";
-import * as ManagedRuntime from "effect/ManagedRuntime";
 import * as Redacted from "effect/Redacted";
 
 import { lambda } from "./aws";
 import { resource } from "./sst";
 
-export const openauthRuntime = Oauth.Openauth.layer({
+export const openauthRuntime = Oauth.Openauth.runtime({
   clientID: Constants.OPENAUTH_CLIENT_IDS.API_GATEWAY,
   fetch: (input) => lambda.fetch(input),
   issuer: resource.Issuer.pipe(Redacted.value).url,
-}).pipe(ManagedRuntime.make);
+});

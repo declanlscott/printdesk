6c1c2360c1a439bd3bfca365e5489dff509fa8ad
Declan L. Scott <declanlscott@gmail.com>
2026-06-16 20:47:31 -0500

    refactor(core): remove licenses policies

--- packages/typescript/core/src/licenses/policies/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Context from "effect/Context";
-
-import type { ServiceShape } from "./layer";
-
-export class LicensesPolicies extends Context.Service<LicensesPolicies, ServiceShape>()(
-  "@printdesk/core/tenants/LicensePolicies",
-) {}

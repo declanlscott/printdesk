6ef9ad46cd57e8e2dc4503a06158add6567c0ee1
Declan L. Scott <declanlscott@gmail.com>
2026-06-22 14:58:37 -0500

    feat(core): tenants schemas

--- packages/typescript/core/src/tenants/contract.ts
+++ packages/typescript/core/src/tenants/contract.ts
@@ -5,6 +5,7 @@ import * as Struct from "effect/Struct";
 import * as HttpServerRespondable from "effect/unstable/http/HttpServerRespondable";
 import * as HttpServerResponse from "effect/unstable/http/HttpServerResponse";
 
+import { CloudflareContract } from "../cloudflare/contract";
 import { ColumnsContract } from "../columns/contract";
 import { Handler } from "../handlers";
 import { IdentityProvidersContract } from "../identity/contract";
@@ -80,30 +81,52 @@ export namespace TenantsContract {
     Output: Table.Dto,
   });
 
-  export class Registration extends Schema.Class<Registration>("Registration")({
-    tenant: Table.Model.mapFields(Struct.pick(["name", "slug", "licenseKey"])),
-    identityProviders: IdentityProvidersContract.Table.Model.mapFields(
-      Struct.pick(["kind", "externalTenantId"]),
-    ).pipe(
-      Schema.NonEmptyArray,
-      Schema.check(
-        Schema.makeFilter((providers) =>
-          Array.length(Array.dedupeWith(providers, (a, b) => a.kind === b.kind)) !==
-          Array.length(providers)
-            ? ["Identity provider kind must be unique"]
-            : [],
+  export class RegistrationPayload extends Schema.Class<RegistrationPayload>("RegistrationPayload")(
+    {
+      tenant: Table.Model.mapFields(Struct.pick(["name", "slug", "licenseKey"])),
+      identityProviders: IdentityProvidersContract.Table.Model.mapFields(
+        Struct.pick(["kind", "externalTenantId"]),
+      ).pipe(
+        Schema.NonEmptyArray,
+        Schema.check(
+          Schema.makeFilter((providers) =>
+            Array.length(Array.dedupeWith(providers, (a, b) => a.kind === b.kind)) !==
+            Array.length(providers)
+              ? ["Identity provider kind must be unique"]
+              : [],
+          ),
         ),
       ),
-    ),
-    papercutConfig: PapercutContract.EnabledConfig.mapFields(Struct.omit(["enabled"])).pipe(
-      Schema.OptionFromOptional,
-    ),
-  }) {}
-
-  export const RegistrationPayload = Registration;
+      papercutConfig: PapercutContract.EnabledConfig.mapFields(Struct.omit(["enabled"])).pipe(
+        Schema.OptionFromOptional,
+      ),
+    },
+  ) {}
 
   export class RegistrationSuccess extends Schema.Class<RegistrationSuccess>("RegistrationSuccess")(
     { deploymentId: EntityId },
     { httpApiStatus: 200 },
   ) {}
+
+  export class SetupPayload extends Schema.Class<SetupPayload>("SetupPayload")({
+    deploymentId: EntityId,
+    papercutApiAuthToken: PapercutContract.ApiAuthToken.pipe(Schema.OptionFromOptional),
+  }) {}
+
+  export class UnexpectedPapercutApiAuthTokenPayloadError
+    extends Schema.TaggedErrorClass<UnexpectedPapercutApiAuthTokenPayloadError>()(
+      "UnexpectedPapercutApiAuthTokenPayloadError",
+      {},
+    )
+    implements HttpServerRespondable.Respondable
+  {
+    public [HttpServerRespondable.symbol] = () =>
+      HttpServerResponse.schemaJson(UnexpectedPapercutApiAuthTokenPayloadError)(this, {
+        status: 400,
+      });
+  }
+
+  export class SetupSuccess extends Schema.Class<SetupSuccess>("SetupSuccess")({
+    papercutApiTunnelToken: CloudflareContract.TunnelToken.pipe(Schema.OptionFromNullOr),
+  }) {}
 }

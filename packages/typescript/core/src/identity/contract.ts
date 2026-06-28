9b272ec0189271caaeccc480a009dab78f3a3a97
Declan L. Scott <declanlscott@gmail.com>
2026-06-28 02:29:53 -0500

    refactor(core): identity providers contract

--- packages/typescript/core/src/identity/contract.ts
+++ packages/typescript/core/src/identity/contract.ts
@@ -1,16 +1,31 @@
 import * as Schema from "effect/Schema";
 import * as SchemaGetter from "effect/SchemaGetter";
 import * as Struct from "effect/Struct";
+import * as HttpServerRespondable from "effect/unstable/http/HttpServerRespondable";
+import * as HttpServerResponse from "effect/unstable/http/HttpServerResponse";
 
+import { CustomerGroupsContract } from "../groups/contracts";
 import { TablesContract } from "../tables/contract";
+import { UsersContract } from "../users/contract";
 import { Constants } from "../utils/constants";
 
 import type { IdentityProvidersTable } from "./sql";
 
 export namespace IdentityProvidersContract {
+  export const Kind = Schema.Literals([Constants.ENTRA_ID, Constants.GOOGLE]);
+  export type Kind = typeof Kind.Type;
+
+  export const Audience = Schema.NonEmptyString.pipe(Schema.brand("IdentityProviderAudience"));
+  export type Audience = typeof Audience.Type;
+
+  export const ExternalTenantId = Schema.NonEmptyString.pipe(
+    Schema.brand("IdentityProviderExternalTenantId"),
+  );
+  export type ExternalTenantId = typeof ExternalTenantId.Type;
+
   export class AccessToken extends Schema.Class<AccessToken>("AccessToken")({
-    audience: Schema.String,
-    tenantId: Schema.String,
+    audience: Audience,
+    tenantId: ExternalTenantId,
   }) {}
 
   export const EntraIdAccessToken = Schema.Struct({
@@ -24,46 +39,60 @@ export namespace IdentityProvidersContract {
   );
 
   export class User extends Schema.Class<User>("User")({
-    id: Schema.String,
-    username: Schema.String,
-    name: Schema.String,
-    email: Schema.String,
+    displayName: UsersContract.DisplayName,
+    email: UsersContract.Email,
+    externalId: UsersContract.ExternalId,
+    username: UsersContract.Username,
   }) {}
 
   export const EntraIdUser = Schema.Struct({
     id: Schema.String,
-    userPrincipalName: Schema.String,
-    preferredName: Schema.String,
     mail: Schema.String,
+    preferredName: Schema.String,
+    userPrincipalName: Schema.String,
   }).pipe(
     Schema.decodeTo(User, {
       decode: SchemaGetter.transform(
         Struct.renameKeys({
-          userPrincipalName: "username",
-          preferredName: "name",
+          id: "externalId",
           mail: "email",
+          preferredName: "displayName",
+          userPrincipalName: "username",
         }),
       ),
       encode: SchemaGetter.transform(
         Struct.renameKeys({
-          username: "userPrincipalName",
-          name: "preferredName",
+          displayName: "preferredName",
           email: "mail",
+          externalId: "id",
+          username: "userPrincipalName",
         }),
       ),
     }),
   );
   export type EntraIdUser = typeof EntraIdUser.Type;
 
-  export const kinds = [Constants.ENTRA_ID, Constants.GOOGLE] as const;
-  export const Kind = Schema.Literals(kinds);
-  export type Kind = typeof Kind.Type;
+  export class Group extends Schema.Class<Group>("Group")({
+    externalId: CustomerGroupsContract.ExternalId,
+    name: CustomerGroupsContract.Name,
+  }) {}
+
+  export const EntraIdGroup = Schema.Struct({
+    displayName: Schema.String,
+    id: Schema.String,
+  }).pipe(
+    Schema.decodeTo(Group, {
+      decode: SchemaGetter.transform(Struct.renameKeys({ displayName: "name", id: "externalId" })),
+      encode: SchemaGetter.transform(Struct.renameKeys({ externalId: "id", name: "displayName" })),
+    }),
+  );
+  export type EntraIdGroup = typeof EntraIdGroup.Type;
 
   export class Table extends TablesContract.Table<IdentityProvidersTable>("identity_providers")(
     {
       ...TablesContract.BaseModel.fields,
-      kind: Schema.Literals(kinds),
-      externalTenantId: Schema.String,
+      kind: Kind,
+      externalTenantId: ExternalTenantId,
     },
     ["create", "read", "delete"],
     [],

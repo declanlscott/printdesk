14bdb34bf97827f7e2b562c044a254640f033759
Declan L. Scott <declanlscott@gmail.com>
2026-07-05 01:34:01 -0500

    refactor(papercut-sync): handler and layer

--- packages/typescript/functions/papercut-sync/src/handler/index.ts
+++ packages/typescript/functions/papercut-sync/src/handler/index.ts
@@ -1,15 +1,19 @@
 import { AccessControl } from "@printdesk/core/access-control";
-import { Actor } from "@printdesk/core/actors";
+import { Actor, ActorLayerMap } from "@printdesk/core/actors";
 import { ActorsContract } from "@printdesk/core/actors/contract";
+import { AppsyncPublisherCredentialIdentityLayerMap } from "@printdesk/core/aws/credential-identity/appsync";
 import { ClientsRepository } from "@printdesk/core/clients/repository";
 import { Config } from "@printdesk/core/config";
 import { Oauth } from "@printdesk/core/oauth";
 import { Openauth } from "@printdesk/core/oauth/openauth";
 import { PapercutSyncer } from "@printdesk/core/papercut/syncer";
+import { ReplicacheNotifier } from "@printdesk/core/replicache/notifier";
 import { TenantsRepository } from "@printdesk/core/tenants/repository";
 import { TenantId } from "@printdesk/core/utils";
+import * as Array from "effect/Array";
 import * as DateTime from "effect/DateTime";
 import * as Effect from "effect/Effect";
+import * as Function from "effect/Function";
 import * as Layer from "effect/Layer";
 import * as Schema from "effect/Schema";
 import * as SchemaGetter from "effect/SchemaGetter";
@@ -24,39 +28,66 @@ export const SystemActorFromEvent = Schema.Struct({ tenantId: TenantId }).pipe(
 
 export const handler = Effect.fn(
   function* (event: typeof SystemActorFromEvent.Encoded) {
-    const systemActor = yield* Schema.decodeEffect(SystemActorFromEvent)(event);
-
+    const accessTokenLayerMap = yield* Oauth.AccessTokenLayerMap;
+    const appsyncPublisherCredentialIdentityLayerMap =
+      yield* AppsyncPublisherCredentialIdentityLayerMap;
+    const actorLayerMap = yield* ActorLayerMap;
+    const config = yield* Config;
+    const clientsRepository = yield* ClientsRepository;
+    const openauth = yield* Openauth.Openauth;
+    const papercutSyncer = yield* PapercutSyncer;
+    const replicacheNotifier = yield* ReplicacheNotifier;
     const tenantsRepository = yield* TenantsRepository;
 
-    const context = yield* Config.use(Struct.get("getPapercutSyncClientCredentials")).pipe(
-      Effect.provideService(Actor, systemActor.wrap),
+    const context = yield* config.getPapercutSyncClientCredentials.pipe(
       Effect.flatMap((credentials) =>
         Effect.all(
           [
-            ClientsRepository.use((repository) =>
-              repository.findById(credentials.id, systemActor.tenantId),
-            ).pipe(Effect.map((client) => new ActorsContract.ClientActor(client).wrap)),
-            Openauth.Openauth.use((openauth) => openauth.clientCredentials(credentials)).pipe(
-              Effect.map((result) => result.tokens.access),
+            openauth
+              .clientCredentials(credentials)
+              .pipe(Effect.map((result) => result.tokens.access)),
+            Actor.use(Struct.get("tenantId")).pipe(
+              Effect.flatMap((tenantId) => clientsRepository.findById(credentials.id, tenantId)),
+              Effect.map((client) => new ActorsContract.ClientActor(client).wrap),
             ),
           ],
           { concurrency: "unbounded" },
         ),
       ),
-      Effect.flatMap(([actor, accessToken]) =>
-        Actor.layer(actor).pipe(Layer.merge(Oauth.AccessToken.layer(accessToken)), Layer.build),
+      Effect.provide(
+        Function.pipe(
+          event,
+          Schema.decodeEffect(SystemActorFromEvent),
+          Effect.map((systemActor) => actorLayerMap.get(systemActor.wrap)),
+          Layer.unwrap,
+        ),
+      ),
+      Effect.flatMap(([accessToken, clientActor]) =>
+        Layer.mergeAll(
+          accessTokenLayerMap.get(accessToken),
+          appsyncPublisherCredentialIdentityLayerMap.get(clientActor),
+        ).pipe(Layer.provideMerge(actorLayerMap.get(clientActor)), Layer.build),
       ),
     );
 
-    yield* PapercutSyncer.use(Struct.get("syncAll")).pipe(
-      AccessControl.enforce(AccessControl.permissionPolicy("papercut_sync:create")),
-      Effect.provideContext(context),
-    );
+    yield* Effect.provideContext(
+      Effect.gen(function* () {
+        yield* AccessControl.permissionPolicy("papercut_sync:create");
 
-    yield* DateTime.now.pipe(
-      Effect.andThen((lastPapercutSyncAt) =>
-        tenantsRepository.updateById(systemActor.tenantId, { lastPapercutSyncAt }),
-      ),
+        yield* papercutSyncer.syncAll.pipe(
+          Effect.map(Array.flatten),
+          Effect.filterOrElse(Array.isArrayEmpty, () => replicacheNotifier.poke),
+          Effect.asVoid,
+        );
+
+        const lastPapercutSyncAt = yield* DateTime.now;
+        yield* Actor.use(Struct.get("tenantId")).pipe(
+          Effect.flatMap((tenantId) =>
+            tenantsRepository.updateById(tenantId, { lastPapercutSyncAt }),
+          ),
+        );
+      }),
+      context,
     );
   },
   (effect) => effect.pipe(Effect.scoped),
--- packages/typescript/functions/papercut-sync/src/handler/layer.ts
+++ packages/typescript/functions/papercut-sync/src/handler/layer.ts
@@ -1,5 +1,8 @@
+import { ActorLayerMap } from "@printdesk/core/actors";
 import { Appconfig } from "@printdesk/core/aws/appconfig";
 import { AppconfigAgent } from "@printdesk/core/aws/appconfig/agent";
+import { AppsyncPublisherCredentialIdentityLayerMap } from "@printdesk/core/aws/credential-identity/appsync";
+import { AppsyncSigner } from "@printdesk/core/aws/sigv4-signers/appsync";
 import * as ClientsRepository from "@printdesk/core/clients/repository/layer";
 import * as Config from "@printdesk/core/config/layer";
 import { Database } from "@printdesk/core/database";
@@ -9,8 +12,11 @@ import { GraphLayerMap } from "@printdesk/core/graph";
 import * as CustomerGroupMembershipsRepository from "@printdesk/core/groups/customer-memberships/repository/layer";
 import * as CustomerGroupsRepository from "@printdesk/core/groups/customers/repository/layer";
 import * as IdentityProvidersRepository from "@printdesk/core/identity/providers-repository/layer";
+import { Oauth } from "@printdesk/core/oauth";
 import * as PapercutApi from "@printdesk/core/papercut/api/layer";
 import * as PapercutSyncer from "@printdesk/core/papercut/syncer/layer";
+import { Realtime } from "@printdesk/core/realtime";
+import * as ReplicacheNotifier from "@printdesk/core/replicache/notifier/layer";
 import * as SharedAccountCustomerAccessRepository from "@printdesk/core/shared-accounts/customer-access/repository/layer";
 import * as SharedAccountCustomerGroupAccessRepository from "@printdesk/core/shared-accounts/customer-group-access/repository/layer";
 import * as SharedAccountsRepository from "@printdesk/core/shared-accounts/repository/layer";
@@ -25,33 +31,38 @@ import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 
 import { openauthLayer } from "../lib/auth";
 
-export const layer = PapercutSyncer.layer.pipe(
-  Layer.merge([
-    ClientsRepository.layer,
-    GraphLayerMap.layer,
-    openauthLayer,
-    TenantsRepository.layer,
-  ]),
+export const layer = Layer.mergeAll(
+  Oauth.AccessTokenLayerMap.layer,
+  AppsyncPublisherCredentialIdentityLayerMap.layer,
+  ActorLayerMap.layer,
+  ClientsRepository.layer,
+  GraphLayerMap.layer,
+  openauthLayer,
+  PapercutSyncer.layer,
+  TenantsRepository.layer,
+  ReplicacheNotifier.layer,
+).pipe(
   Layer.provide([
     CustomerGroupMembershipsRepository.layer,
     CustomerGroupsRepository.layer,
     IdentityProvidersRepository.layer,
     PapercutApi.layer,
+    Realtime.layer,
     SharedAccountCustomerAccessRepository.layer,
     SharedAccountCustomerGroupAccessRepository.layer,
     SharedAccountsRepository.layer,
     UsersRepository.layer,
   ]),
   Layer.provideMerge([Config.layer]),
-  Layer.provide([SyncQueryBuilder.layer, XmlRpc.XmlRpc.layer]),
   Layer.provide([
     Appconfig.layer,
     AppconfigAgent.layer,
-    Database.layer,
-    Xml.Builder.layer,
-    Xml.Parser.layer,
+    AppsyncSigner.layer,
+    SyncQueryBuilder.layer,
+    XmlRpc.XmlRpc.layer,
   ]),
+  Layer.provide([Database.layer, Xml.Builder.layer, Xml.Parser.layer]),
   Layer.provide([Drizzle.layerWithDrizzleServices, FetchHttpClient.layer]),
   Layer.provide(PgClient.layer),
-  Layer.provide(SstResource.layer),
+  Layer.provideMerge(SstResource.layer),
 );

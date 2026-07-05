be9b9dc69978f5a87232d357eec8c1e9a2d2b391
Declan L. Scott <declanlscott@gmail.com>
2026-07-05 01:16:59 -0500

    refactor(core): actor layer

--- packages/typescript/core/src/actors/index.ts
+++ packages/typescript/core/src/actors/index.ts
@@ -10,7 +10,7 @@ export class Actor extends Context.Service<Actor, ActorsContract.Actor>()(
   "@printdesk/core/actors/Actor",
 ) {
   public static readonly layer = (actor: typeof Actor.Service) =>
-    Layer.succeed(this, this.of(actor));
+    Layer.succeed(this, this.of(actor)).pipe(Layer.fresh);
 }
 
 export class ActorLayerMap extends LayerMap.Service<ActorLayerMap>()(

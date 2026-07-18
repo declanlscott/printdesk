68ec5e1ebe708a3be64209cc9c97cc3e9e401ffc
Declan L. Scott <declanlscott@gmail.com>
2026-07-18 16:27:26 -0500

    refactor(core): entra id auth provider

--- packages/typescript/core/src/identity/entra-id.ts
+++ packages/typescript/core/src/identity/entra-id.ts
@@ -3,7 +3,6 @@ import { AzureIdentityAuthenticationProvider } from "@microsoft/kiota-authentica
 import * as Context from "effect/Context";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
-import * as LayerMap from "effect/LayerMap";
 import * as Redacted from "effect/Redacted";
 import * as Schema from "effect/Schema";
 import * as Struct from "effect/Struct";
@@ -11,51 +10,50 @@ import * as Struct from "effect/Struct";
 import { SstResource } from "../sst/resource";
 import { Constants } from "../utils/constants";
 
+import type { AccessToken } from "@azure/identity";
 import type { IdentityProvidersContract } from "./contract";
 
 export namespace EntraId {
-  export class CredentialError extends Schema.TaggedErrorClass<CredentialError>()(
-    "EntraIdCredentialError",
-    { cause: Schema.Defect() },
-  ) {}
-
   export class AuthProviderError extends Schema.TaggedErrorClass<AuthProviderError>()(
     "EntraIdAuthProviderError",
     { cause: Schema.Defect() },
   ) {}
 
   // @effect-leakable-service
-  export class AuthProvider extends Context.Service<AuthProvider>()(
-    "@printdesk/core/identity/entra-id/AuthProvider",
-    {
-      make: Effect.fn(function* (externalTenantId: IdentityProvidersContract.ExternalTenantId) {
-        const { clientId, clientSecret } = yield* SstResource.useSync((resource) =>
-          resource.IdentityProviders.pipe(Redacted.value, Struct.get(Constants.ENTRA_ID)),
-        );
-
-        const credential = yield* Effect.try({
-          try: () => new ClientSecretCredential(externalTenantId, clientId, clientSecret),
-          catch: (cause) => new CredentialError({ cause }),
-        });
-
-        const provider = yield* Effect.try({
-          try: () => new AzureIdentityAuthenticationProvider(credential),
-          catch: (cause) => new AuthProviderError({ cause }),
-        });
-
-        return { externalTenantId, provider } as const;
+  export class AuthProvider extends Context.Service<
+    AuthProvider,
+    AzureIdentityAuthenticationProvider
+  >()("@printdesk/core/identity/entra-id/AuthProvider") {
+    public static readonly fromAccessToken = Effect.fn((accessToken: AccessToken) =>
+      Effect.try({
+        try: () => new AzureIdentityAuthenticationProvider({ getToken: async () => accessToken }),
+        catch: (cause) => new AuthProviderError({ cause }),
       }),
-    },
-  ) {
-    public static readonly layer = (...args: Parameters<typeof this.make>) =>
-      this.make(...args).pipe(Layer.effect(this), Layer.fresh);
-  }
+    );
+    public static readonly fromClientCredentials = Effect.fn(function* (
+      tenantId: IdentityProvidersContract.ExternalId,
+    ) {
+      const { clientId, clientSecret } = yield* SstResource.useSync((resource) =>
+        resource.IdentityProviders.pipe(Redacted.value, Struct.get(Constants.ENTRA_ID)),
+      );
 
-  export class AuthProviderLayerMap extends LayerMap.Service<AuthProviderLayerMap>()(
-    "@printdesk/core/identity/entra-id/AuthProviderLayerMap",
-    {
-      dependencies: [SstResource.layer],
-      lookup: AuthProvider.layer,
-    },
-  ) {}
+      const credential = yield* Effect.try({
+        try: () => new ClientSecretCredential(tenantId, clientId, clientSecret),
+        catch: (cause) => new AuthProviderError({ cause }),
+      });
+
+      return yield* Effect.try({
+        try: () => new AzureIdentityAuthenticationProvider(credential),
+        catch: (cause) => new AuthProviderError({ cause }),
+      });
+    });
+
+    public static readonly layerFromAccessToken = (
+      ...args: Parameters<typeof this.fromAccessToken>
+    ) => this.fromAccessToken(...args).pipe(Layer.effect(this), Layer.fresh);
+
+    public static readonly layerFromClientCredentials = (
+      ...args: Parameters<typeof this.fromClientCredentials>
+    ) => this.fromClientCredentials(...args).pipe(Layer.effect(this), Layer.fresh);
+  }
 }

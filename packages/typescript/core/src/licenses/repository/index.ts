7b9eee17fad6b257ed2723ac799c049b4ac40c9e
Declan L. Scott <declanlscott@gmail.com>
2026-07-18 03:23:55 -0500

    refactor(core): licenses repository service tag

--- packages/typescript/core/src/licenses/repository/index.ts
+++ packages/typescript/core/src/licenses/repository/index.ts
@@ -3,5 +3,5 @@ import * as Context from "effect/Context";
 import type { ServiceShape } from "./layer";
 
 export class LicensesRepository extends Context.Service<LicensesRepository, ServiceShape>()(
-  "@printdesk/core/tenants/LicensesRepository",
+  "@printdesk/core/licenses/Repository",
 ) {}
